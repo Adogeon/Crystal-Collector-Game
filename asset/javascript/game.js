9ca2a6cdafee3f8a8239b0e495d717d60f1f7105
Anh Dao <addao823@gmail.com>
2019-03-16 13:52:04 -0700

    Battery Low! Emergency. Working on reset button

--- asset/javascript/game.js
+++ asset/javascript/game.js
@@ -5,8 +5,8 @@ var loses = 0;
 
 //declare the crystal;
 var crystal1,crystal2,crystal3,crystal4
-var crystals = [];
-var targetNum = 0;
+var crystalsMap;
+var targetScore = 0;
 var playerScore = 0;
 var state = 0;
 class crystal {
@@ -14,6 +14,7 @@ class crystal {
         this.id = str;
         this.htmlEl = this.getCrystalHTMLEl();
         this.value = (Math.floor(Math.random()*11))+1;
+        this.valueDisplay = false;
     }
     getCrystalHTMLEl() {
         var IDStr = "#"+this.id;
@@ -34,59 +35,57 @@ function newGame() {
     crystal3 = new crystal("crystal-3");
     crystal4 = new crystal("crystal-4");
 
-    crystals = [crystal1,crystal2,crystal3,crystal4]
-
-    while (targetNum === 0) {//prevent edge case of this just equal to 0
-        targetNum = crystal1.value*randomMulNum() + crystal2.value*randomMulNum() + crystal3.value*randomMulNum() + crystal4.value*randomMulNum()
+    crystalsMap = {
+        "crystal-1":crystal1,
+        "crystal-2":crystal2,
+        "crystal-3":crystal3,
+        "crystal-4":crystal4
     }
 
-    $("#target-num").append(targetNum);
+    while (targetScore === 0) {//prevent edge case of this just equal to 0
+        targetScore = crystal1.value*randomMulNum() + crystal2.value*randomMulNum() + crystal3.value*randomMulNum() + crystal4.value*randomMulNum()
+    }
+
+    $("#target-num").append(targetScore);
 }
 
-function updateScore(crystalEl) {
-    var newValue = 0;
-    console.log(crystalEl.id);
-    crystals.forEach(function(element) {
-        console.log(element.getCrystalHTMLEl().id);
-        if(element.id === crystalEl.id) {
-            newValue = element.value;
-        }
-    })
+function updateScore(crystalDOM) {
+    var newValue = crystalsMap[crystalDOM.id].value;
     playerScore += newValue;
-    
+    $("#score-num").text(playerScore);
 }
 
-function
+function displayCrystalValue(crystalDOM) {
+    var tempID = crystalDOM.id;
+    if(!crystalsMap[tempID].valueDisplay) {
+        $(crystalDOM).append(crystalsMap[tempID].value);
+        crystalsMap[tempID].valueDisplay = true;
+    }
+}
+
+function checkForWin() {
+    if(playerScore === targetScore) {//win condition
+        state = 1;
+        wins++;
+        $("#win-number").text(wins);
+    } else if(playerScore > targetScore) {
+        state = 1;
+        loses++;
+        $("#lose-number").text(loses);
+    }
+    if(state === 1) {
+        $("#reset").hidden = false;
+    }
+}
 
 // Assign the value of crystal to the crystal HTML Element 
 newGame();
 $(".crystal").on("click", function() {
-    if (state < 2) { 
-        if(state === 0) {
-            // shown the crystal value & add the score to value
-            console.log("clicking");
-            console.log(this.id);
-            switch (this.id) {
-                case "crystal-1":
-                    //show crystal value
-                    //problem: how to display the value, on top of the crystal or somewhere else ?
-                    $(this).append(crystal1.value);
-                    break;
-                case "crystal-2":
-                    $(this).append(crystal2.value);
-                    break;
-                case "crystal-3":
-                    $(this).append(crystal3.value);
-                    break;
-                case "crystal-4":
-                    $(this).append(crystal4.value);
-                    break;
-            }
-            state = 1; 
-        }   
+    var isFinished = state === 1;
+    if (!isFinished) { 
+        displayCrystalValue(this); 
         updateScore(this);
         checkForWin();
-        
     }
 })
 
